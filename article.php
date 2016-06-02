28d807edf605624673b731a8fc09a0bd92a90a5f
Mat Marquis <mat@matmarquis.com>
2016-06-02 16:52:07 -0400

    Some copy in the demo pages.

--- article.php
+++ article.php
@@ -1,5 +1,4 @@
 <?php include( "functions/criticalcss.php" ); ?>
-
 <!DOCTYPE html>
 	<html dir="ltr" lang="en-US">
 	<head>
@@ -8,5 +7,9 @@
 	</head>
 	<body>
 		<h1>Hello there</h1>
+
+		<p>This page doesn’t have a CriticalCSS file associated with it, but uses the same loading pattern as all the other pages. In the event that a CriticalCSS file isn’t available for a page with a given slug, the full stylesheet is loaded as usual.</p>
+
+		<p>This page can be added to the CriticalCSS pattern by adding a new entry in <code>/tasks/criticalcss.js</code>.</p>
 	</body>
 </html>
--- category.php
+++ category.php
@@ -1,5 +1,4 @@
 <?php include( "functions/criticalcss.php" ); ?>
-
 <!DOCTYPE html>
 	<html dir="ltr" lang="en-US">
 	<head>
@@ -7,6 +6,8 @@
 		<?php load_critical_css( "category" ); ?>
 	</head>
 	<body>
-		<h1 class="category-hed">Hello there</h1>
+		<h1 class="category-hed">A Category Page</h1>
+
+		<p>This page has a unique style for its heading, so that style will be included in the page’s CriticalCSS.</p>
 	</body>
 </html>
