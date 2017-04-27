81aa6bc98f7ef46a3b04df4d6f4e33edcda52cc0
deathbearbrown <deathbearbrown@gmail.com>
2017-04-27 12:43:23 -0700

    basic webpack that builds things

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,54 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 // Path definitions
-var buildRoot = path.resolve(__dirname, '../../');
+var buildRoot = path.resolve(__dirname, './');
 
 // CriticalCSS Plugins
 var critical = require('webpack-plugin-critical');
-
-// Environment flags
-var build = process.env.NODE_ENV === 'build';
-var isProduction = process.argv.indexOf('-p') !== -1;
-var isHot = process.argv.indexOf('--hot') !== -1;
 
 // Plugins
 var plugins = [
 ];
 
-// Define webpack bundles
-var webpackBundles = {
-	index: './app/homepage/index.js',
-	article: './app/article/index.js',
-	category: './app/category/index.js'
-};
-
 // Main webpack config
 module.exports = {
-
+  entry: {
+    index: './app/homepage/index.js',
+    article: './app/article/index.js',
+    category: './app/category/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: buildRoot,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      }
+    ]
+  },
+  plugins: [
+    new critical.CriticalPlugin({
+      src: 'index.html',
+      base: path.resolve(__dirname),
+      inline: true,
+      minify: true,
+      dest: 'index.html'
+    }),
+    new ExtractTextPlugin('css/[name].css')
+  ]
 }
